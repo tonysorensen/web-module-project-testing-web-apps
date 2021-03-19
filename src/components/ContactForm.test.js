17ae3c364e9648d8cc7608599a632db7a15cd56f
Tony Sorensen <tony48853@gmail.com>
2021-03-18 20:59:11 -0700

    ok, first error message works, really.

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {render, screen, waitFor } from '@testing-library/react';
+import {
+    render,
+    screen,
+    waitFor
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import ContactForm from './ContactForm';
 
-test('renders without errors', ()=>{
-   render(<ContactForm/>); 
+test('renders without errors', () => {
+    render( < ContactForm / > );
 });
 
-test('renders the contact form header', ()=> {
-    render (<ContactForm/>)
+test('renders the contact form header', () => {
+    render( < ContactForm / > )
     const header = screen.getByText(/contact form/i)
     expect(header).toBeTruthy();
     expect(header).toBeInTheDocument();
@@ -19,35 +23,41 @@ test('renders the contact form header', ()=> {
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
     //First render the form. Then get the element. Then do the thing to the element then check the result! so grab the firstname field, simulate putting text in it and see what happens
-    render(<ContactForm/>)
-    const firstName = screen.getByPlaceholderText("Edd");
+    render( < ContactForm / > )
+    const firstNameInput = screen.getByLabelText("First Name*");
 
+    userEvent.type(firstNameInput, "Tony");
+    const error = screen.getByTestId("error")
+    expect(error).toBeInTheDocument();
 
-    if (firstName.length<5){
-        expect(() => render(<ContactForm />)).toThrow()
-    }
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+    // render(<ContactForm/>)
+    // const firstName = screen.getByPlaceholderText("Edd");
+    // const lastName = screen.getByPlaceholderText("Burke")
+    // const email = screen.getByLabelText("Email*")
+    // if(firstName.length <1 && lastName.length<1 && email.length>1){
+    //     expect(()=> render(<ContactForm/>)).toThrow()
+    // }
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
+
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
+
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    
+
 });
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-    
+
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
-    
+
 });
